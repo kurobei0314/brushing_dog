10c07c254923d7d506f732dbc37f94d245adfce3
kurobei0314 <maikurodei.biseibutu@gmail.com>
2022-05-07 20:47:59 +0900

    ゲームプレイ時間の調整

--- brushing_dog/Assets/Scripts/Constant.cs
+++ brushing_dog/Assets/Scripts/Constant.cs
@@ -11,7 +11,7 @@ public static class GameInfo{
     public static readonly Vector3 DEFAULT_POSITION = new Vector3(1000,1000,1000);
 
     // ゲームのプレイ時間
-    public static readonly float GAMETIMES = 5.0f;
+    public static readonly float GAMETIMES = 20.0f;
 
     // ゲーム開始のカウント時間
     public static readonly float START_GAME_COUNTER = 4.0f;
