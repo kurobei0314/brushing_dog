8f44137a476b7c50c66a597590c12449874574f0
kurobei0314 <maikurodei.biseibutu@gmail.com>
2022-05-08 14:05:45 +0900

    アニメーションの追加

--- brushing_dog/Assets/Scripts/StartController.cs
+++ brushing_dog/Assets/Scripts/StartController.cs
@@ -2,20 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StartController : MonoBehaviour
 {
     [SerializeField]
-    GameObject text1,text2,text3;
+    Animator titleAnimator;
+
+    [SerializeField]
+    GameObject text1;
+
+    [SerializeField]
+    GameObject text2;
+
+    [SerializeField]
+    GameObject text3;
+
+    [SerializeField]
+    GameObject text4;
+
+    int titleFlg = 0;
 
     // Update is called once per frame
+    void Awake(){
+        text1.SetActive(false);
+        text2.SetActive(false);
+        text3.SetActive(false);
+        text4.SetActive(false);
+    }
+
     void Update()
     {
-        StartCoroutine("StartAnimation");
+        if(titleFlg == 0){
+            // StartCoroutine("TitleAnimation");
+            titleAnimator.Play("TitleAnimation");
+            titleFlg = 1;
+        }
     }
-    private IEnumerator StartAnimation(){
+
+    private IEnumerator TitleAnimation() {
+        // titleAnimator.Play("TitleAnimation");
+        // yield return new WaitForSeconds (5.0f);
+
+        text1.SetActive(true);
+        AudioManager.Instance.PlaySE("don");
+        yield return new WaitForSeconds (0.5f);
+        text1.SetActive(false);
+        text2.SetActive(true);
+        AudioManager.Instance.PlaySE("don");
+        yield return new WaitForSeconds (0.5f);
+        text2.SetActive(false);
+        text3.SetActive(true);
+        AudioManager.Instance.PlaySE("don");
+        yield return new WaitForSeconds (0.5f);
+        text3.SetActive(false);
+        text4.SetActive(true);
+        AudioManager.Instance.PlaySE("dodon");
+        yield return new WaitForSeconds (2.5f);
         
-        yield return new WaitForSeconds (1.0f);
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("Main");
     }
+
 }
